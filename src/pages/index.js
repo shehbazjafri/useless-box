a661beb1a5d5d81f31cc73f97cd5904b530f39b3
shehbazjafri <galaxytwirls@gmail.com>
2020-07-11 17:32:13 +0530

    add switch sound with light up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 import SEO from "../components/seo"
 import styled from "styled-components"
 import "../components/app.css"
 import SwitchImg from "../images/switchBlack.svg"
+import SwitchSound from "../assets/switch-5.mp3"
 
 const AppContainer = styled.div`
   background: #5583ac;
@@ -12,9 +13,7 @@ const AppContainer = styled.div`
   align-content: center;
 `
 
-const Box = styled.div`
-  /* margin: auto; */
-`
+const Box = styled.div``
 
 const SwitchContainer = styled.div`
   position: absolute;
@@ -44,35 +43,50 @@ const BoxBottom = styled.div`
 `
 
 const Divider = styled.div`
-  /* background: linear-gradient(#6e5c48, #6e5c48) no-repeat center/2px 100%; */
   border-right: 2px solid #6e5c48;
 `
 
 const Switch = styled.img`
   position: absolute;
   top: -30%;
-  /* left: 10%; */
-  right: 15%;
-  /* transform: rotate(0deg); */
-  /* filter: invert(53%) sepia(15%) saturate(4283%) hue-rotate(322deg)
-    brightness(106%) contrast(95%); */
-  filter: invert(50%) sepia(10%) saturate(2199%) hue-rotate(314deg)
-    brightness(98%) contrast(92%);
+  right: ${props => (props.isSwitchedOn ? `0%` : `25%`)};
+  transform: ${props =>
+    props.isSwitchedOn ? `rotate(45deg)` : `rotate(0deg)`};
+  filter: ${props =>
+    props.isSwitchedOn
+      ? `invert(53%) sepia(15%) saturate(4283%) hue-rotate(322deg)
+    brightness(106%) contrast(95%)`
+      : `invert(50%) sepia(10%) saturate(2199%) hue-rotate(314deg)
+    brightness(98%) contrast(92%)`};
 `
 
-const IndexPage = () => (
-  <AppContainer>
-    <SEO title="App" />
-    <Box>
-      <BoxTop>
-        <SwitchContainer>
-          <Switch src={SwitchImg} width="30px" alt="Red Switch" />
-        </SwitchContainer>
-        <Divider />
-      </BoxTop>
-      <BoxBottom />
-    </Box>
-  </AppContainer>
-)
+const App = () => {
+  const [isSwitchedOn, setIsSwitchedOn] = useState(false)
+  const switchSound = new Audio(SwitchSound)
 
-export default IndexPage
+  const handleSwitch = () => {
+    switchSound.play()
+    setIsSwitchedOn(!isSwitchedOn)
+  }
+  return (
+    <AppContainer>
+      <SEO title="App" />
+      <Box>
+        <BoxTop>
+          <SwitchContainer>
+            <Switch
+              src={SwitchImg}
+              alt="Red Switch"
+              onClick={handleSwitch}
+              isSwitchedOn={isSwitchedOn}
+            />
+          </SwitchContainer>
+          <Divider />
+        </BoxTop>
+        <BoxBottom />
+      </Box>
+    </AppContainer>
+  )
+}
+
+export default App
