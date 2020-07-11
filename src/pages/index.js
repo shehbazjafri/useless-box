8ae20d456c93774b42efee796ea3f9056461278e
shehbazjafri <galaxytwirls@gmail.com>
2020-07-11 14:03:13 +0530

    initial setup, styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "styled-components"
+import "../components/app.css"
+
+const AppContainer = styled.div`
+  background: #5583ac;
+  height: 100%;
+`
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <AppContainer>
+    <SEO title="App" />
+  </AppContainer>
 )
 
 export default IndexPage
