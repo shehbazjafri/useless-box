c2476bdcaa73a6f0b7e259eee8f2d176d636292f
shehbazjafri <galaxytwirls@gmail.com>
2020-07-12 21:30:43 +0530

    update description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Useless Box`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `A fun and useless box`,
+    author: `Shehbaz Jafri`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
